95c036adb036cf71e1b8c928b15dec61095d79b9
NangMoonLaung <nangmoonlaung.ml@gmail.com>
2026-03-09 21:34:09 +0630

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import folium
+from streamlit_folium import st_folium
 
+st.title("Yangon IoT Sensor Map")
+
+# Yangon location example
+lat = 16.8661
+lon = 96.1951
+
+m = folium.Map(location=[lat, lon], zoom_start=12)
+
+folium.Marker(
+    [lat, lon],
+    popup="Sensor Location"
+).add_to(m)
+
+st_folium(m, width=700, height=500)
